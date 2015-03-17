b40c7b629e0ec9e03da71aae187ff69aa347bb9a
Släggan <slaggan@gmail.com>
2015-03-17 15:34:44 +0100

    New attempt on new branch

--- HDinnerParty/src/main.java
+++ HDinnerParty/src/main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.LinkedList;
+import java.util.List;
 
 
 class main {
@@ -30,9 +31,9 @@ class DinnerReader {
 		for(int i = 0;i<noGuests;i++) {
 			extractIngredients();
 //			prettyPrint();
-			Solution sol = findCombo(targetValue);
+			List<Integer> sol = findCombo(targetValue);
 			if(sol != null) {
-				for(Integer j : sol.ingredients){
+				for(Integer j : sol){
 					System.out.print(alfabet.charAt(j));
 				}
 				System.out.print("\n");
@@ -68,25 +69,25 @@ class DinnerReader {
 			System.out.println(" And amount: " + amountArray[i]);
 		}
 	}
-	public Solution findCombo(int targetValue) {
-		LinkedList<Solution> solutionList = new LinkedList<Solution>();
-		solutionList.add(new Solution(targetValue));
-		
+	public LinkedList<Integer> findCombo(int targetValue) {
+		LinkedList<Integer> solutionList = new LinkedList<Integer>();
+		//solutionList.add(new Solution(targetValue));
+		int remainsTarget = targetValue;
 		for (int i = 0; i < valueArray.length; i++) {
-			int remainsCurrent = amountArray[i];
-			while (remainsCurrent > 0) {
-				LinkedList<Solution> tempList = new LinkedList<Solution>();
-				for(Solution s : solutionList) {
-					Solution old = s.add(valueArray[i], i);
-					if(old != null) {
-						if(s.isComplete){
-							return s;
-						}
-						tempList.add(old);
+			int remainsElementInLoop = amountArray[i];
+			
+			while (remainsElementInLoop > 0) {
+
+				if(remainsTarget >= valueArray[i]) {
+					remainsElementInLoop--;
+					remainsTarget-= valueArray[i];
+					solutionList.add(i);
+					if(remainsTarget == 0) {
+						return solutionList;
 					}
 				}
-				solutionList.addAll(tempList);
-				remainsCurrent--;
+				else {break;}
+				
 			}
 		}
 		return null;
